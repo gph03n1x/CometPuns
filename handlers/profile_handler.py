15cd2dacfbad468064fe30d7aad5cdaba04dea31
seven7scanner <dominicgfour@yahoo.gr>
2016-02-25 12:36:04 +0200

    added test_avatar method (line 17) ++immediate revise is needed++
    Signed-off-by: SeVeN

--- handlers/profile_handler.py
+++ handlers/profile_handler.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import tornado.web
+import re
 import logging
 
 from handlers.base_handler import BaseHandler
@@ -12,3 +13,11 @@ class ProfileHandler(BaseHandler):
         if user is None:
             user = self.get_secure_cookie("user")
         self.render("profile.html")
+
+    def test_avatar(self, test_file):
+        self.r_image = self.re._compile(r".*\.(jpg|jpeg|jpe|png|pns|gif|)")
+
+        if self.r_image.match(test_file):
+            pass
+        else:
+            self.logging.debug('This in not a valid image type')
