6c42eae8527ab39f6b9391bc394ec9c34b8a77c6
Giannis Papaioannou <giannispapcod7@gmail.com>
2016-02-28 16:48:11 +0200

    Created puns2test.py script which will create dummy openers and responses

---
+++ puns2test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import sqlite3
+
+connection = sqlite3.connect("database/puns.db")
+cursor = connection.cursor()
+for i in range(100):
+    placeholder = "Placeholder #%s" % (str(i))
+    cursor.execute("INSERT INTO openers (content) VALUES (?)", (placeholder,))
+    connection.commit()
+    cursor.execute("INSERT INTO responses (content) VALUES (?)", (placeholder,))
+    connection.commit()
+    
+connection.close()
+print "DONE"
