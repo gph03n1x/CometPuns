415fcf1a55df799a8629948df5181bb3f9dcdfd7
Giannis Papaioannou <giannispapcod7@gmail.com>
2016-09-28 20:36:08 +0300

    xrange, doesnt exist. A room thought it had more users than it truly did

--- engine/storage/puns.py
+++ engine/storage/puns.py
@@ -20,48 +20,48 @@ class databasePuns:
             # This means our database is empty
             import sys
             sys.exit("[-]Please add openers and responses first")
-        
-        
+
+
     def createTables(self):
         # creates sql tables
         self.execute_raw("CREATE TABLE IF NOT EXISTS openers (id INTEGER PRIMARY KEY, content)")
         self.execute_raw("CREATE TABLE IF NOT EXISTS responses (id INTEGER PRIMARY KEY, content)")
-        
-        
+
+
     def count_openers(self):
         # counts the number of openers there are in the database
         self.cursor.execute("SELECT Count(*) FROM openers")
         rows = self.cursor.fetchone()
         return rows[0]
-    
-    
+
+
     def get_response_by_id(self, response_id):
         self.cursor.execute("SELECT * FROM responses WHERE id=?",(response_id,))
         return self.cursor.fetchone()
-    
-    
-    
+
+
+
     def count_responses(self):
         # counts the number of responses there are in the database
         self.cursor.execute("SELECT Count(*) FROM openers")
         rows = self.cursor.fetchone()
         return rows[0]
-    
-    
+
+
     def get_random_opener(self):
         # generates a random number between 1 and the number of rows
         # and then we fetch the row with id = generater number
         random_id = random.randint(1, self.openers)
         self.cursor.execute("SELECT * FROM openers WHERE id=?",(random_id,))
         return self.cursor.fetchone()
-    
-    
+
+
     def generate_random_responses(self, number_of_players):
         # Create a population of ids from 0<=id<=self.responses
         # and fetch a sample of number_of_players*self.options_per_player
         logging.debug(number_of_players)
         logging.debug(number_of_players*self.options_per_player)
-        id_pool = random.sample(xrange(self.responses), number_of_players*self.options_per_player)
+        id_pool = random.sample(range(self.responses), number_of_players*self.options_per_player)
         result_pool = []
         for random_id in id_pool:
             # for each id we fetch their response from the
@@ -69,8 +69,8 @@ class databasePuns:
             self.cursor.execute("SELECT * FROM responses WHERE id=?",(random_id,))
             result_pool.append(self.cursor.fetchone())
         return result_pool
-    
-    
+
+
     def execute_raw(self, *query):
         # method for executing queries and fetching the
         # lastrowid so we can do that in one line and we
@@ -78,7 +78,7 @@ class databasePuns:
         self.cursor.execute(*query)
         self.connection.commit()
         return self.cursor.lastrowid
-    
-    
+
+
 if __name__ == '__main__':
     pun = databasePuns("database/puns.db")
